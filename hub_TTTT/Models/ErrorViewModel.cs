b06ea39e1e876a81f382f929364091485b49fac2
KhanhNV <KhanhNV@fpts.com.vn>
2026-03-17 14:13:49 +0700

    thêm testhub cho marketPool

---
+++ hub_TTTT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hub_TTTT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
